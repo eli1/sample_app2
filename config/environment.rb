49ed4bf33c16b54088feae62fdf0e0ab52844c0b
ElisabethJanssen <eli-janssen@gmx.de>
2014-12-04 21:36:35 +0100

    Finish user signup

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+SampleAppKursWs14::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleAppKursWs14::Application.config.secret_token = '43e0a6ac4b1a4dc05bfc052ab722c49ef8e9360346c66f24cf32d89bf1e37bc46baae5095b4a41ddec4715cdd7228a03d48f3fbc232aa802978a966d316f4c0b'
